575ed7dc163a83a79cc764ff5d494dd6b58216f1
Mamun <mdmmn378@gmail.com>
2023-10-25 14:12:34 +0600

    Added animation

---
+++ frontend/src/components/Animation.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="flex justify-center items-center">
+        <div id="bars" class="flex justify-center items-center" v-show="!startAnimation">
+            <div v-for="i in  10 " class="bar" :style="{ animationDuration: '0ms' }">
+            </div>
+        </div>
+        <div id="bars" class="flex sm:hidden justify-center items-center" v-show="startAnimation">
+            <div v-for=" i  in  10 " class="bar" :style="{ animationDuration: randomDuration(700, 200) + 'ms' }">
+            </div>
+        </div>
+        <div id="bars" class="hidden md:flex justify-center items-center" v-show="startAnimation">
+            <div v-for=" i  in  20 " class="bar" :style="{ animationDuration: randomDuration(700, 200) + 'ms' }">
+            </div>
+        </div>
+        <div id="bars" class="hidden lg:flex justify-center items-center" v-show="startAnimation">
+            <div v-for=" i  in  22 " class="bar" :style="{ animationDuration: randomDuration(700, 200) + 'ms' }">
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script  setup>
+const props = defineProps({
+    startAnimation: Boolean
+})
+
+function randomDuration(mean, std) {
+    let u = 0;
+    let v = 0;
+    while (u === 0) u = Math.random();
+    while (v === 0) v = Math.random();
+    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2 * Math.PI * v);
+    const randomValue = mean + z * std;
+    return randomValue;
+
+}
+</script>
+  
+<style scoped>
+#bars {}
+
+.bar {
+    background: #52467b;
+    bottom: 1px;
+    height: 3px;
+    width: 10px;
+    margin: 0px 4px;
+    border-radius: 5px;
+    animation: sound 0ms 600ms linear infinite alternate;
+}
+
+@keyframes sound {
+    0% {
+        opacity: .35;
+        height: 3px;
+    }
+
+    100% {
+        opacity: 1;
+        height: 70px;
+    }
+}
+</style>
+  
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div class="flex items-center justify-center">
+
+      <div class="h-92 w-4/5 p-6 m-6 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md shadow-md">
+        <h1 class="text-3xl font-bold text-white text-center">Shobdo</h1>
+        <div class="flex flew-row w-full">
+          <p class="text-white text-center m-6 p-2 space-x-10 w-full justify-center">Select your model: </p>
+          <select
+            class="w-full h-10 px-3 m-6 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline justify-center items-center self-center"
+            v-model="selected">
+            <option disabled value="" class="text-sm">Select Model</option>
+            <option v-for="option in options" :value="option.value" class="text-sm">
+              {{ option.text }}
+            </option>
+          </select>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <!-- <textarea
+            class="w-full h-32 px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline m-2"
+            placeholder="Enter your text here"></textarea> -->
+          <Animation :startAnimation="true" class="m-6 p-6 h-20 w-full"></Animation>
+          <button
+            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow ">
+            Generate
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const selected = ref('')
+
+const options = [
+  { text: 'Alberta', value: 'alberta' },
+  { text: 'Bert', value: 'bert' },
+  { text: 'Roberta', value: 'roberta' },
+]
+
+</script>
