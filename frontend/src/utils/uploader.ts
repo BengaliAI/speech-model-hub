7809bf1e346f9076ad1bc259efdcbf77952b0e8f
Mamun <mdmmn378@gmail.com>
2023-10-29 04:58:30 +0600

    Added CORS middleware

---
+++ frontend/src/utils/uploader.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { v4 as uuid } from 'uuid';
+
+
+export const uploadAudio = async (blob: Blob, displayName: string) => {
+    const formData = new FormData();
+    const fileName = `${uuid()}.wav`;
+    formData.append("file", blob, fileName);
+    formData.append("display_name", displayName);
+    const response = await axios.post('http://localhost:8080/api/v1/models/inference', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+    return response.data;
+    };
+
